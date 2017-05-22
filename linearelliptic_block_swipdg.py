01929f85b4c2f2b2057940067d8d39ed3f6e1a90
Stephan Rave <stephanrave@uni-muenster.de>
2017-05-22 17:26:58 +0200

    failing example

--- linearelliptic_block_swipdg.py
+++ linearelliptic_block_swipdg.py
@@ -26,7 +26,7 @@ def discretize():
         make_boundary_info_on_dd_subdomain_layer as make_boundary_info,
         make_apply_on_inner_intersections_2d_simplex_aluconformgrid_dd_subdomain_coupling_part as make_apply_on_inner_intersections  # NOQA
     )
-    from dune.xt.functions import make_constant_function_1x1, make_constant_function_2x2, make_expression_function_1x1
+    from dune.xt.functions import make_constant_function_1x1, make_constant_function_2x2, make_expression_function_1x1, make_checkerboard_function_1x1
     from dune.xt.la import (
         IstlRowMajorSparseMatrixDouble as Matrix,
         IstlDenseVectorDouble as Vector,
@@ -44,7 +44,7 @@ def discretize():
     init_logger()
 
     inner_boundary_id = 18446744073709551573
-    grid = make_grid(lower_left=[-1, -1], upper_right=[1, 1], num_elements=[4, 4], num_refinements=4,
+    grid = make_grid(lower_left=[0, 0], upper_right=[1, 1], num_elements=[4, 4], num_refinements=4,
                      num_partitions=[2, 2], num_oversampling_layers=0, inner_boundary_segment_index=inner_boundary_id)
     grid.visualize('../block_swipdg_esv2007_grid', with_coupling=True)
 
@@ -52,9 +52,16 @@ def discretize():
                                               'default': 'dirichlet',
                                               'neumann': '[{} {}]'.format(inner_boundary_id, inner_boundary_id+1)})
 
-    lmbda = make_constant_function_1x1(grid, 1.0, name='diffusion')
+    # lmbda = make_constant_function_1x1(grid, 1.0, name='diffusion')
+    XBLOCKS = YBLOCKS = 2
+    values = [[0.1]]*(YBLOCKS*XBLOCKS)
+    values[0] = [1.]
+    lmbda = make_checkerboard_function_1x1(grid_provider=grid, lower_left=[0, 0], upper_right=[1, 1],
+                                           num_elements=[XBLOCKS, YBLOCKS],
+                                           values=values, name='diffusion')
     kappa = make_constant_function_2x2(grid, [[1., 0.], [0., 1.]], name='diffusion')
-    f = make_expression_function_1x1(grid, 'x', '0.5*pi*pi*cos(0.5*pi*x[0])*cos(0.5*pi*x[1])', order=3, name='force')
+    # f = make_expression_function_1x1(grid, 'x', '0.5*pi*pi*cos(0.5*pi*x[0])*cos(0.5*pi*x[1])', order=3, name='force')
+    f = make_expression_function_1x1(grid, 'x', '1', order=3, name='force')
 
     block_space = make_block_space(grid)
 
@@ -165,11 +172,11 @@ def discretize():
     return d
 
 d = discretize()
-# d.visualize(d.solve(), filename='foo')
 
 from pymor.reductors.system import GenericRBSystemReductor
 
 U = d.solve()
+d.visualize(U, filename='U')
 bases = {b.space.id: b.copy() for b in U._blocks}
 reductor = GenericRBSystemReductor(d, bases)
 rd = reductor.reduce()
