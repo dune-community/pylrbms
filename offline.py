2bb5cbd991614fc7d240c88ffc9b3af43be3c4a3
Felix Schindler <felix.schindler@wwu.de>
2017-06-13 00:01:10 +0200

    [scripts] move RB init into separater module

---
+++ offline.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from dune.xt.la import IstlDenseVectorDouble as Vector
+from dune.gdt import make_discrete_function, project
+
+from pymor.bindings.dunext import DuneXTVector
+from pymor.reductors.system import GenericRBSystemReductor
+from pymor.vectorarrays.list import ListVectorArray
+
+
+def init_local_reduced_bases(d, block_space, order):
+    if order > 1:
+        order = 1
+    U = d.solution_space.empty()
+    reductor = GenericRBSystemReductor(d, products=[d.operators['local_energy_dg_product_{}'.format(ii)]
+                                                    for ii in range(block_space.num_blocks)])
+    if order >= 0:
+        print('initializing local reduced bases with DG shape functions of up to order {} ... '.format(order), end='')
+
+        # order 0 basis
+        for ii in range(block_space.num_blocks):
+            local_space = block_space.local_space(ii)
+            reductor.extend_basis_local(ListVectorArray([DuneXTVector(Vector(local_space.size(), 1.)), ],
+                                                        U._blocks[ii].space))
+    if order >= 1:
+        for ii in range(block_space.num_blocks):
+            local_space = block_space.local_space(ii)
+            tmp_discrete_function = make_discrete_function(local_space)
+            for expression in ('x[0]', 'x[1]', 'x[0]*x[1]'):
+                func = make_expression_function_1x1(grid, 'x', expression, order=2)
+                project(func, tmp_discrete_function)
+                reductor.extend_basis_local(ListVectorArray([DuneXTVector(tmp_discrete_function.vector_copy()), ],
+                                                            U._blocks[ii].space))
+            del tmp_discrete_function
+
+    print('done')
+    return reductor
+
