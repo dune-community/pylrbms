7f0ddc0817c0339064459914ccee44f1b998b419
Felix Schindler <felix.schindler@wwu.de>
2017-12-05 13:27:23 +0100

    [scripts] update to parabolic convergence study

--- parabolic_convergence_study.py
+++ parabolic_convergence_study.py
@@ -21,12 +21,9 @@ def discretize(grid_and_problem_data, T, nt):
     return d, {'block_space': data['block_space'], 'unblock': d.unblock}
 
 
-print('Block SWIPDG P1, dt = 0.1*h')
-print()
-
 base_cfg = {'num_coarse_grid_elements': [4, 4],
             'num_grid_refinements': 2,
-            'num_grid_subdomains': [1, 1],
+            'num_grid_subdomains': [2, 2],
             'num_grid_oversampling_layers': 1,
             'T': 1}
 base_cfg['dt'] = 0.1 * init_grid_and_problem(base_cfg)['grid'].max_entity_diameter()
@@ -36,12 +33,18 @@ for level in range(InstationaryEocStudy.max_levels + 1):
     reference_cfg = refine(reference_cfg)
 reference_cfg['dt'] = 0.1 * init_grid_and_problem(reference_cfg)['grid'].max_entity_diameter()
 
+mu = (1, 1, 1, 1)
+print('Thermalblock, mu={}, Block SWIPDG P1, dt = 0.1*h'.format(mu))
+print('===========================================================')
+print()
+
 parabolic_study = InstationaryEocStudy(
         init_grid_and_problem,
         discretize,
         base_cfg,
         refine,
         reference_cfg,
-        mu=(1, 1, 1, 1))
-parabolic_study.run(('h', 'L_oo - elliptic_mu_bar', 'eta_nc', 'eta_r', 'eta_df', 'R_T', 'partial_t_nc', 'eta'))
+        mu=mu)
+parabolic_study.run(('h', 'eta_nc', 'eta_r', 'eta_df', 'R_T', 'partial_t_nc'))
+print()
 
