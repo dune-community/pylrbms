f042fd8d8c67543769f44e972ed99a3220dafe8a
Felix Schindler <felix.schindler@wwu.de>
2017-12-06 18:11:09 +0100

    [scripts...parabolic_swipdg] strip time from stationary parameter type

--- discretize_parabolic_swipdg.py
+++ discretize_parabolic_swipdg.py
@@ -1,6 +1,7 @@
 from pymor.algorithms.timestepping import ImplicitEulerTimeStepper
 from pymor.bindings.dunegdt import DuneGDTVisualizer
 from pymor.discretizations.basic import InstationaryDiscretization
+from pymor.parameters.spaces import CubicParameterSpace
 
 from discretize_elliptic_swipdg import discretize as discretize_stationary
 
@@ -8,6 +9,8 @@ from discretize_elliptic_swipdg import discretize as discretize_stationary
 def discretize(grid_and_problem_data, T, nt, polorder):
 
     d, data = discretize_stationary(grid_and_problem_data, polorder)
+    assert isinstance(d.parameter_space, CubicParameterSpace)
+    parameter_range = grid_and_problem_data['parameter_range']
 
     d = InstationaryDiscretization(T,
                                    d.operator.source.zeros(1),
@@ -19,8 +22,8 @@ def discretize(grid_and_problem_data, T, nt, polorder):
                                    products=d.products,
                                    operators={kk: vv for kk, vv in d.operators.items()
                                               if kk not in ('operator', 'rhs') and vv not in (d.operators, d.rhs)},
-                                   parameter_space=d.parameter_space,
                                    visualizer=DuneGDTVisualizer(data['space']))
+    d = d.with_(parameter_space=CubicParameterSpace(d.parameter_type, parameter_range[0], parameter_range[1]))
 
     return d, data
 
