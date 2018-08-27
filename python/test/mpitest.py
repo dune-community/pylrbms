cf2e23273666a083d0cb6d1b89796a3e22dae795
René Milk <rene.milk@wwu.de>
2018-08-27 15:09:34 +0200

    [test] adds a couple of py tests

--- python/test/mpitest.py
+++ python/test/mpitest.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 import sys
+import tempfile
+
 from mpi4py import MPI
 import numpy
+
+from dune.xt.common.test import runmodule
 
 def test_make_grid():
     comm = MPI.COMM_WORLD
@@ -21,3 +25,25 @@ def test_make_grid():
     domain=([0, 0], [1, 1])
     grid = make_grid(domain[0], domain[1], num_elements=[32, 32], num_refinements=0, overlap_size=[1, 1],
                      mpi_comm=comm_split)
+
+
+def test_new_comm():
+    if MPI.COMM_WORLD.Get_size() == 1:
+        mpi_comm = MPI.COMM_WORLD.Clone()
+    else:
+        comm = MPI.COMM_WORLD.Clone()
+        mpi_comm = comm.Split(color=(comm.Get_rank() == 0), key=comm.Get_rank())
+        assert mpi_comm.Get_size() < MPI.COMM_WORLD.Get_size()
+    mpi_comm.Set_errhandler(MPI.ERRORS_ARE_FATAL)
+
+
+def test_blockspace():
+    from pymor.vectorarrays.block import BlockVectorSpace, BlockVectorArray
+    from pymor.core.pickle import dump, load
+
+    b = BlockVectorSpace([])
+    with tempfile.TemporaryFile('wb') as dp_file:
+        dump(b, file=dp_file)
+
+if __name__ == '__main__':
+    runmodule(__file__)
